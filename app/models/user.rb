82a77c4c393f8864f05694ba5e227b207688d5e9
khyeh <kevin.yay@gmail.com>
2015-07-21 07:55:36 +0000

    made sure @example..com is invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase } # callback that downcases the email so that Example.CoM and example.com are the same
+  before_save { email.downcase! } # callback that downcases the email so that Example.CoM and example.com are the same
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 } 
+  # for the two lines right above,
+  # It turns out the has_secure_password method includes a presence validation, 
+  # but it only applies to new records, which causes problems when updating users.
 end
